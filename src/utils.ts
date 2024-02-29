ddd7b90e90dc6ad035b9003c6e0f87bb368640fd
vimo <39148877+vimode@users.noreply.github.com>
2024-02-29 17:55:23 +0530

    Add dummy data for empty load

--- src/utils.ts
+++ src/utils.ts
@@ -61,3 +61,57 @@ export function compareObjects (inputDataObject:InputFormData, allQueries:UserQu
   }
   return null
 }
+
+//dummy data
+export const chartDummyData: UserQuery = {
+  "id": "15e113df-605d-488a-9069-08e32cf70468",
+  "query": {
+    "initialDeposit": 1000,
+    "rateOfInterest": 2,
+    "yearsOfGrowth": 5
+  },
+  "details": [
+    {
+      "year": 0,
+      "rateOfInterest": 2,
+      "initialDeposit": 1000,
+      "currentAmount": 1000,
+      "increasingInterest": 0
+    },
+    {
+      "year": 1,
+      "rateOfInterest": 2,
+      "initialDeposit": 1000,
+      "currentAmount": 1020,
+      "increasingInterest": 20
+    },
+    {
+      "year": 2,
+      "rateOfInterest": 2,
+      "initialDeposit": 1000,
+      "currentAmount": 1040.4,
+      "increasingInterest": 20.400000000000002
+    },
+    {
+      "year": 3,
+      "rateOfInterest": 2,
+      "initialDeposit": 1000,
+      "currentAmount": 1061.208,
+      "increasingInterest": 20.808000000000003
+    },
+    {
+      "year": 4,
+      "rateOfInterest": 2,
+      "initialDeposit": 1000,
+      "currentAmount": 1082.43216,
+      "increasingInterest": 21.22416
+    },
+    {
+      "year": 5,
+      "rateOfInterest": 2,
+      "initialDeposit": 1000,
+      "currentAmount": 1104.0808032,
+      "increasingInterest": 21.648643200000002
+    }
+  ]
+}
